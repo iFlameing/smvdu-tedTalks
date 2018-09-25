d20cb2bcbae46737f85d8ca4e618647190f355ad
iFlameing <ialokkumarsingh0@gmail.com>
2018-09-25 22:38:39 +0530

    adding the routes

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app =express()
 
 
+app.set('view engine','ejs')
 app.use(express.static("public"))
 
-app.get('/in',(req,res)=>{
-    res.send("This is file coming soon")
+app.get('/',(req,res)=>{
+    res.render("imageslider.ejs")
+})
+
+app.get('/team',(req,res)=>{
+    res.render('contributors.ejs');
+})
+
+app.get('/speakers',(req,res)=>{
+    res.render('speakers.ejs')
+})
+app.get('/about',(req,res)=>{
+    res.render('about.ejs')
 })
 
 app.listen(process.env.PORT,function(){
